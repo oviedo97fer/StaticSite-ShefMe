f4535bef4062768895aaf8dd09d2e73f4e3e701b
oviedo97fer <fernando.n.oviedo@gmail.com>
2019-09-23 00:34:17 -0300

    Updates

---
+++ precache-manifest.228f51041d3ec1f7c8630ac0ed4eb8ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3c451bf3610a7897afd70d6c431aa12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4334f17c49f080b6804",
+    "url": "/static/css/main.f6cb7cf9.chunk.css"
+  },
+  {
+    "revision": "ebce351b4d7027fb6988",
+    "url": "/static/js/2.fa8bde0f.chunk.js"
+  },
+  {
+    "revision": "a4334f17c49f080b6804",
+    "url": "/static/js/main.ee13bf81.chunk.js"
+  },
+  {
+    "revision": "3900b8ae0402431b9ea9",
+    "url": "/static/js/runtime-main.0e23a98b.js"
+  },
+  {
+    "revision": "8ada39c9965257d11d3b94925913f7f2",
+    "url": "/static/media/img.8ada39c9.jpg"
+  }
+]);
