58570c392e973f5cb24ee937b807c562b917cc51
oviedo97fer <fernando.n.oviedo@gmail.com>
2019-09-23 00:32:37 -0300

    Finished

---
+++ shef-me-static/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import ShefMe from './ShefMe.png'
+function App() {
+  return (
+    <div className="App">
+      <img src={ShefMe} alt='ShefMe'/>
+    </div>
+  );
+}
+
+export default App;
